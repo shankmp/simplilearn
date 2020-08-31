049ff0d5b8fe60eb28238d2d7745cf9d83ffa430
Ubuntu <shankarpille@yahoo.co.in>
2020-08-31 06:25:20 +0000

    Dockerizing Jenkins Pipeline first commit

---
+++ dockerizing-jenkins-pipeline/src/main/java/com/simplilearn/test/DockerizingJenkinsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizingJenkinsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizingJenkinsPipelineApplication.class, args);
+	}
+
+}
